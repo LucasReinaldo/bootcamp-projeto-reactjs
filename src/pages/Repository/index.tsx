f9b9a04aae3643f673e46ae0458c3821900a323c
Lucas Reinaldo de Melo <lucasreinaldo.demelo@hotmail.com>
2020-04-29 12:37:50 +0100

    Navegando entre rotas

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <div />;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return <h1>Repo: {params.repository}</h1>;
 };
 
 export default Repository;
